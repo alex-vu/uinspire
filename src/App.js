e1ba2defba12cb5930d2c56189432b2bbab7d565
Alex <alex.vu6@outlook.com>
2019-03-12 22:42:15 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo-2.svg";
+import "./App.css";
+import Bricks from "brick.js";
+
+const sizes = [
+  { columns: 2, gutter: 10 },
+  { mq: "768px", columns: 3, gutter: 25 },
+  { mq: "1024px", columns: 4, gutter: 50 }
+];
+
+// const brickInstance = Bricks({
+//   container: ".grid",
+//   packed: "data-packed",
+//   position: false,
+//   sizes: sizes
+// });
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="header-wrap">
+            <div className="App-logo">
+              <a href="#">
+                <img src={logo} alt="logo" />
+              </a>
+            </div>
+            <div className="search-container f-auto">
+              <label className="search-label">
+                <input type="search" placeholder="Search" />
+              </label>
+            </div>
+            <ul className="nav-wrapper d-flex">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Following
+                </a>
+              </li>
+            </ul>
+          </div>
         </header>
+        <main className="page-content">
+          <div className="container">
+            <div className="grid" />
+          </div>
+        </main>
       </div>
     );
   }
